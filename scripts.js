db3a717a6d7f841768747c1102d1824731b1c96c
juniormusasizi61 <juniormusasizi6@gmail.com>
2024-08-21 15:03:59 +0300

    js

--- scripts.js
+++ scripts.js
@@ -2,3 +2,10 @@ const todoValue = document.getElementById('todoText');
 const todoAlert  = document.getElementById('Alert');
 const listitem  = document.getElementById('list-items');
 const addUpdate = document.getElementById('AddUpdateClick');
+
+
+//declare local storage object
+let todo = JSON.parse(localStorage.getItem("todo-list"));
+if (!todo) {
+  todo = [];
+}
