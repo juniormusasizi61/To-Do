6ef8d35ba55a5172f541e9c0584f7ce41dc76adb
juniormusasizi61 <juniormusasizi6@gmail.com>
2024-08-21 15:01:35 +0300

    js functionality

---
+++ scripts.js
@@ -0,0 +1,4 @@
+const todoValue = document.getElementById('todoText');
+const todoAlert  = document.getElementById('Alert');
+const listitem  = document.getElementById('list-items');
+const addUpdate = document.getElementById('AddUpdateClick');
