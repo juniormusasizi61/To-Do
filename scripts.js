a6756ee5d238dcbff2288601e85544e14b2a7281
juniormusasizi61 <juniormusasizi6@gmail.com>
2024-08-21 15:30:07 +0300

    js CRUD

--- scripts.js
+++ scripts.js
@@ -9,3 +9,39 @@ let todo = JSON.parse(localStorage.getItem("todo-list"));
 if (!todo) {
   todo = [];
 }
+
+
+///CRUD funtions 
+function CreateToDoItems(){
+    if(todoValue.value ===""){
+        todoAlert.innerText = "please enter your task!...";
+        todoValue.focus();
+    }else{
+        let IsPresent = false;
+        todo.forEach(element => {
+            if(element.item == todoValue.value){
+                IsPresent = true;
+            }
+        });
+    if(IsPresent){
+        setAlertMessage('This item already exists on the list!');
+        return;
+    }
+    let li = document.createElement('li');
+    const todoItems = '<div title = "hit doudle click and complete' ondblclick = "CompletedToDoItems(this)">${todoValue.value}</div><div><img class ="edit to-do controls" onclick="UpdateToDoItems(this)" src = "./img/pencil.png" />
+    <img class = "delete todo-controls" onclick="DeleteToDoItems(this)" src = "./img/delete.png"/>
+    </div>
+
+    li.innerHTML = todoItems;
+    listItems.appendChild(li);
+
+    if(!todo){
+        todo = [];
+    }
+    let itemList = {item: todoValue.value, status: false};
+    todo.push(itemList);
+    setLocalStorage();
+    }
+todoValue.value= '';
+setAlertMessage('Todo item Created Successfully!');
+}
